38f0cba6d6ab1b29b65d49eb19a7f56dd115442c
hodulayahweh-wq <hodulayahweh@gmail.com>
2026-01-21 01:29:49 +0300

    Update main.py

--- main.py
+++ main.py
@@ -156,4 +156,4 @@ async def shutdown():
 
 # ================== RUN ==================
 if __name__ == "__main__":
-    uvicorn.run("main:app", host="0.0.0.0", port=PORT)
+    uvicorn.run("app:app", host="0.0.0.0", port=PORT)
